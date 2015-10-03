ddf9a2875009ae40a3b286575cfb866e0ab6de38
Umut Celenli <umut@celenli.com>
2015-10-03 16:40:15 +0300

    Added Npgsql.EntityFramework and EntityFramework packages Added db connectionstring and DbContext

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Service/Entities/.dbcontext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishMaster.Service.Entities
+{
+    public class WishMasterDataContext : DbContext
+    {
+        public WishMasterDataContext()
+            : base("wishmaster")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();           
+        }
+    }
+}
