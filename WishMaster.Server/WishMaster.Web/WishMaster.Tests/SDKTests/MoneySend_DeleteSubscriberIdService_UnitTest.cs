8bd399f6265e725a82012e6ac9d12cd295e357c3
Umut Celenli <umut@celenli.com>
2015-10-03 16:39:29 +0300

    MoneySend tests

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Tests/SDKTests/MoneySend_DeleteSubscriberIdService_UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterCard.SDK.Services.MoneySend;
+using MasterCard.SDK;
+using MasterCard.SDK.Services.MoneySend.Domain;
+using WishMaster.Service.Tools;
+
+namespace Test.Services
+{
+    [TestClass]
+    public class DeleteSubscriberIdService_UnitTest
+    {
+        private DeleteSubscriberIdService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            service = new DeleteSubscriberIdService(Security.GetConsumerKey(), Security.GetPrivateKey(), Environments.Environment.SANDBOX);
+        }
+
+        [TestMethod]
+        public void TestDeleteSubscriberIdRequest()
+        {
+            DeleteSubscriberIdRequest deleteSubscriberIdRequest = new DeleteSubscriberIdRequest();
+            deleteSubscriberIdRequest.SubscriberId = "example@email.com";
+            deleteSubscriberIdRequest.SubscriberType = "PHONE_NUMBER";
+            DeleteSubscriberId deleteSubscriberId = service.GetDeleteSubscriberId(deleteSubscriberIdRequest);
+            Assert.IsTrue(deleteSubscriberId != null);
+        }
+
+    }
+}
---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Tests/SDKTests/MoneySend_PanEligibilityService_UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterCard.SDK.Services.MoneySend;
+using MasterCard.SDK;
+using MasterCard.SDK.Services.MoneySend.Domain;
+using WishMaster.Service.Tools;
+
+namespace Test.Services
+{
+    [TestClass]
+    public class PanEligibilityService_UnitTest
+    {
+        PanEligibilityService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            service = new PanEligibilityService(Security.GetConsumerKey(), Security.GetPrivateKey(), Environments.Environment.SANDBOX);
+        }
+
+        [TestMethod]
+        public void TestPanEligibilityService_NotEligible()
+        {
+            PanEligibilityRequest panEligibilityRequest = new PanEligibilityRequest();
+            panEligibilityRequest.SendingAccountNumber = 5184680990000024L;
+            panEligibilityRequest.ReceivingAccountNumber = 5184680060000201L;
+            PanEligibility panEligibility = service.GetPanEligibility(panEligibilityRequest);
+            Assert.IsTrue(panEligibility != null);
+            Assert.IsTrue(panEligibility.RequestId != null && panEligibility.RequestId > 0);
+            Assert.IsTrue(panEligibility.SendingEligibility.AccountNumber > 0);
+            Assert.IsTrue(panEligibility.ReceivingEligibility.AccountNumber > 0);
+            Assert.IsTrue(panEligibility.SendingEligibility.Eligible == false);
+            Assert.IsTrue(panEligibility.SendingEligibility.AccountNumber > 0);
+            Assert.IsTrue(panEligibility.RequestId > 0);
+            Assert.IsTrue(panEligibility.ReceivingEligibility.Eligible == false);
+            Assert.IsTrue(panEligibility.ReceivingEligibility.AccountNumber > 0);
+        }
+
+        [TestMethod]
+        public void TestPanEligibilityServiceSending_Eligible()
+        {
+            PanEligibilityRequest panEligibilityRequest = new PanEligibilityRequest();
+            panEligibilityRequest.SendingAccountNumber = 5184680430000006L;
+            PanEligibility panEligibility = service.GetPanEligibility(panEligibilityRequest);
+            Assert.IsTrue(panEligibility != null);
+            Assert.IsTrue(panEligibility.RequestId != null && panEligibility.RequestId > 0);
+            Assert.IsTrue(panEligibility.SendingEligibility.AccountNumber > 0);
+        }
+
+        [TestMethod]
+        public void TestPanEligibilityServiceReceiving_Eligible()
+        {
+            PanEligibilityRequest panEligibilityRequest = new PanEligibilityRequest();
+            panEligibilityRequest.ReceivingAccountNumber = 5184680430000006L;
+            PanEligibility panEligibility = service.GetPanEligibility(panEligibilityRequest);
+            Assert.IsTrue(panEligibility != null);
+            Assert.IsTrue(panEligibility.RequestId != null && panEligibility.RequestId > 0);
+            Assert.IsTrue(panEligibility.ReceivingEligibility.AccountNumber > 0);
+        }
+    }
+}
