08a8151b36b9fee18f1f9dd76926c216bbc0c652
Umut Celenli <umut@celenli.com>
2015-10-03 20:31:09 +0300

    CardService and ProductService added

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Service/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WishMaster.Service.Entities;
+
+namespace WishMaster.Service.Services
+{
+    public class ProductService : BaseService
+    {
+        public ProductService(WishMasterDataContext db)
+            :base(db)
+        {
+
+        }
+
+    }
+}
