1487122741e4788f20f17fbb4dad523860bf578a
Umut Celenli <umut@celenli.com>
2015-10-03 17:22:41 +0300

    Entities created, DbContext updated with entities DbMigrations enabled

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Service/Entities/FraudLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishMaster.Service.Entities
+{
+    public class FraudLog
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+    
+        public long CardId { get; set; }
+        [ForeignKey("CardId")]
+        public Card Card { get; set; }
+    }
+}
