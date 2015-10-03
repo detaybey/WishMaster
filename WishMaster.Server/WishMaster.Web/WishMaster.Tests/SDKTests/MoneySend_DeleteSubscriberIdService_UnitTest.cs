0bbb1b8b94f376b5920ef91704cf194ee2ea0a20
Umut Celenli <umut@celenli.com>
2015-10-03 19:10:36 +0300

    Added SimplifyCommerce API DLL Added SimpliftCommerce API Tests

--- WishMaster.Server/WishMaster.Web/WishMaster.Tests/SDKTests/MoneySend_DeleteSubscriberIdService_UnitTest.cs
+++ WishMaster.Server/WishMaster.Web/WishMaster.Tests/SDKTests/MoneySend_DeleteSubscriberIdService_UnitTest.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MasterCard.SDK.Services.MoneySend;
-using MasterCard.SDK;
-using MasterCard.SDK.Services.MoneySend.Domain;
-using WishMaster.Service.Tools;
+﻿//using System;
+//using System.Text;
+//using System.Collections.Generic;
+//using System.Linq;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using MasterCard.SDK.Services.MoneySend;
+//using MasterCard.SDK;
+//using MasterCard.SDK.Services.MoneySend.Domain;
+//using WishMaster.Service.Tools;
 
-namespace Test.Services
-{
-    [TestClass]
-    public class DeleteSubscriberIdService_UnitTest
-    {
-        private DeleteSubscriberIdService service;
+//namespace Test.Services
+//{
+//    [TestClass]
+//    public class DeleteSubscriberIdService_UnitTest
+//    {
+//        private DeleteSubscriberIdService service;
 
-        [TestInitialize]
-        public void Init()
-        {
-            service = new DeleteSubscriberIdService(Security.GetConsumerKey(), Security.GetPrivateKey(), Environments.Environment.SANDBOX);
-        }
+//        [TestInitialize]
+//        public void Init()
+//        {
+//            service = new DeleteSubscriberIdService(Security.GetConsumerKey(), Security.GetPrivateKey(), Environments.Environment.SANDBOX);
+//        }
 
-        [TestMethod]
-        public void TestDeleteSubscriberIdRequest()
-        {
-            DeleteSubscriberIdRequest deleteSubscriberIdRequest = new DeleteSubscriberIdRequest();
-            deleteSubscriberIdRequest.SubscriberId = "example@email.com";
-            deleteSubscriberIdRequest.SubscriberType = "PHONE_NUMBER";
-            DeleteSubscriberId deleteSubscriberId = service.GetDeleteSubscriberId(deleteSubscriberIdRequest);
-            Assert.IsTrue(deleteSubscriberId != null);
-        }
+//        [TestMethod]
+//        public void TestDeleteSubscriberIdRequest()
+//        {
+//            DeleteSubscriberIdRequest deleteSubscriberIdRequest = new DeleteSubscriberIdRequest();
+//            deleteSubscriberIdRequest.SubscriberId = "example@email.com";
+//            deleteSubscriberIdRequest.SubscriberType = "PHONE_NUMBER";
+//            DeleteSubscriberId deleteSubscriberId = service.GetDeleteSubscriberId(deleteSubscriberIdRequest);
+//            Assert.IsTrue(deleteSubscriberId != null);
+//        }
 
-    }
-}
+//    }
+//}
