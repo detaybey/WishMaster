2af57b65c2b9d97d23e28f30daa6638102d52dc2
Umut Celenli <umut@celenli.com>
2015-10-03 15:17:30 +0300

    Added security class to services

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Service/Tools/Security.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishMaster.Service.Tools
+{
+    public static class Security
+    {
+        public static X509Certificate2 GetCertificate()
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (var cert in store.Certificates)
+            {
+                if (cert.HasPrivateKey && cert.Issuer.Contains("MasterCard"))
+                {
+                    return cert;
+                }
+            }
+            return null;
+        }
+
+    }
+}
