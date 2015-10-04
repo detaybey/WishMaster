970463c9549dfc97401492c53d1d5a77ee12660f
EmreAbi <emre@celenli.com>
2015-10-04 04:03:26 +0300

    Main Page Layout Adjustment

--- WishMaster.Server/WishMaster.Web/WishMaster.Web/js/main.js
+++ WishMaster.Server/WishMaster.Web/WishMaster.Web/js/main.js
@@ -7,21 +7,51 @@ angular.module('app')
     function ($scope, $window) {
 
 
-         
+
 
         $(document).ready(function () {
-        $('.ui.menu .ui.dropdown').dropdown({
-            on: 'hover'
-        }
+            $('.ui.menu .ui.dropdown').dropdown({
+                on: 'hover'
+            }
 
-        );
+            );
 
-        //$('.ui.menu a.item')
-        //  .on('click', function () {
-        //      $(this)
-        //        .addClass('active').siblings().removeClass('active')
-        //      ;
-        //  });
-    })
+            $('.ui.form').form({
+             fields: {
+                 email: {
+                     identifier: 'email',
+                     rules: [
+                       {
+                           type: 'empty',
+                           prompt: 'Please enter your e-mail'
+                       },
+                       {
+                           type: 'email',
+                           prompt: 'Please enter a valid e-mail'
+                       }
+                     ]
+                 },
+                 password: {
+                     identifier: 'password',
+                     rules: [
+                       {
+                           type: 'empty',
+                           prompt: 'Please enter your password'
+                       },
+                       {
+                           type: 'length[6]',
+                           prompt: 'Your password must be at least 6 characters'
+                       }
+                     ]
+                 }
+             }
+         });
+            //$('.ui.menu a.item')
+            //  .on('click', function () {
+            //      $(this)
+            //        .addClass('active').siblings().removeClass('active')
+            //      ;
+            //  });
+        })
 
     }]);
