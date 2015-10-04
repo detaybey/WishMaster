2538f023999a6c395fefcff710891fe481f5c052
Umut Celenli <umut@celenli.com>
2015-10-04 06:15:01 +0300

    Partial views for model binding on home page added

---
+++ WishMaster.Server/WishMaster.Web/WishMaster.Service/ViewModels/HomePageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WishMaster.Service.Entities;
+
+namespace WishMaster.Service.ViewModels
+{
+   public class HomePageModel
+    {
+        public List<Product> Products = new List<Product>();
+        public List<Category> Categories = new List<Category>();
+        public List<User> TopUsers = new List<User>();
+        public long SelectedCategoryId = 0;
+    }
+}
